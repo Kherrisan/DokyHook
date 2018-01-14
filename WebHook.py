d9af123ccb0cf94d4e119e7362eaf53de3572cef
Dokyme <zdkscope@qq.com>
2018-01-14 17:08:14 +0800

    Pass offline test and add some annotation.

--- WebHook.py
+++ WebHook.py
@@ -4,13 +4,24 @@ import hmac
 
 
 class WebHookInjector(object):
+    """
+
+    """
 
     @staticmethod
     def inject(url, app, webhook, secret=None):
-        if secret:
-            webhook.hmac_o = hmac.new(bytes(secret))
+        """
+
+        :param url:
+        :param app:
+        :param webhook:
+        :param secret:
+        :return:
+        """
         if isinstance(webhook, type):
             webhook = webhook.__call__()
+        if secret:
+            webhook.hmac_o = hmac.new(bytes(secret, "utf-8"))
 
         class WebHookHandler(tornado.web.RequestHandler):
             def post(self, *args, **kwargs):
@@ -25,6 +36,11 @@ class AbstractWebHook(object):
         self.hmac_o = None
 
     def handle_post(self, request):
+        """
+
+        :param request:
+        :return:
+        """
         if not request.headers:
             return
         headers = request.headers
@@ -43,11 +59,16 @@ class AbstractWebHook(object):
             return
         payload = request.body
         callback = self.__dispatch(event_type)
-        if not callback:
-            callback(self, payload)
+        if callback:
+            callback(payload)
         return
 
     def __dispatch(self, event_type):
+        """
+
+        :param event_type:
+        :return:
+        """
         if event_type == "commit_comment":
             return self.on_commit_comment
         elif event_type == "create":
@@ -60,37 +81,103 @@ class AbstractWebHook(object):
             return None
 
     def on_commit_comment(self, payload):
+        """
+        Any time a Commit is commented on.
+        :param payload:
+        :return:
+        """
         pass
 
     def on_create(self, payload):
+        """
+        Any time a Branch or Tag is created.
+        :param payload:
+        :return:
+        """
         pass
 
     def on_delete(self, payload):
+        """
+        Any time a Branch or Tag is deleted.
+        :param payload:
+        :return:
+        """
         pass
 
     def on_fork(self, payload):
+        """
+        Any time a Repository is forked.
+        :param payload:
+        :return:
+        """
         pass
 
     def on_issue_comment(self, payload):
+        """
+        Any time a comment on an issue is created, edited, or deleted.
+        :param payload:
+        :return:
+        """
         pass
 
     def on_issue(self, payload):
+        """
+        Any time an Issue is assigned, unassigned, labeled, unlabeled, opened, edited,
+        milestoned, demilestoned, closed, or reopened.
+        :param payload:
+        :return:
+        """
         pass
 
-    def on_labele(self, payload):
+    def on_label(self, payload):
+        """
+        Any time a Label is created, edited, or deleted.
+        :param payload:
+        :return:
+        """
         pass
 
     def on_milestone(self, payload):
+        """
+        Any time a Milestone is created, closed, opened, edited, or deleted.
+        :param payload:
+        :return:
+        """
         pass
 
     def on_pull_request(self, payload):
+        """
+        Any time a pull request is assigned, unassigned, labeled, unlabeled, opened,
+        edited, closed, reopened, or synchronized (updated due to a new push in the
+        branch that the pull request is tracking).
+        Also any time a pull request review is requested, or a review request is removed.
+        :param payload:
+        :return:
+        """
         pass
 
     def on_push(self, payload):
+        """
+        Any Git push to a Repository, including editing tags or branches.
+        Commits via API actions that update references are also counted.
+        This is the default event.
+        :param payload:
+        :return:
+        """
         pass
 
     def on_release(self, payload):
+        """
+        Any time a Release is published in a Repository.
+        :param payload:
+        :return:
+        """
         pass
 
     def on_watch(self, payload):
+        """
+        Any time a User stars a Repository.
+        :param payload:
+        :return:
+        """
         pass
